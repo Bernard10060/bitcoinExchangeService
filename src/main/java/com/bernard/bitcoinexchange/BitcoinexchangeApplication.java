f83d3dab7021870da97e03623e0f7cb705312c38
bernard <bernard10060@yahoo.com>
2019-02-13 13:39:24 +0800

    初始化

---
+++ src/main/java/com/bernard/bitcoinexchange/BitcoinexchangeApplication.java
@@ -0,0 +1,14 @@
+package com.bernard.bitcoinexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoinexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitcoinexchangeApplication.class, args);
+	}
+
+}
+
